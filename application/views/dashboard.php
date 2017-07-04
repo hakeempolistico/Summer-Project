e23e349de6291b3c74a8e2d03c819e00b11410c0
adriitakumi <nicolettekagamine@gmail.com>
2017-07-04 11:46:33 +0800

    stuff

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -31,6 +31,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         "showpercentintooltip": "0",
         "bgcolor": "#FFFFFF",
         "basefontcolor": "#400D1B",
+        "use3dlighting": "0",
         "showshadow": "0",
         "animation": "1",
         "showborder": "0",
@@ -104,25 +105,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 		  			<div class="w3-col w3-container"><center>
 		  				<div class = "w3-row">
-							<div class = "w3-col" style = "width:25%;" >
+							<div class = "w3-col w3-mobile m3 l3" style = "width:25%;" >
 								<div class="w3-card-4 w3-center w3-border w3-animate-left" onclick="document.getElementById('id01').style.display='block'" style="width:240px; height: 160px; background-color: #bdd2e5; animation-duration: 1.5s;">					
 								<i class="fa fa-tint fa-5x" aria-hidden="true" style="font-size: 80px; margin: 20px 0 20px 0; color: #4681b4"></i>
 								<div class="w3-container w3-center" style="height: 40px; font-size: 25px; background-color: #4681b4; color: white; ">PAID</div>
 								</div>
 							</div>
-							<div class = "w3-col" style = "width:25%;">
+							<div class = "w3-col w3-mobile m3 l3" style = "width:25%;">
 								<div class="w3-card-4 w3-center w3-border w3-animate-left" style="width:240px; height: 160px; background-color: #f2cd75; animation-duration: 1.5s;">
 								<i class="fa fa-internet-explorer fa-5x" aria-hidden="true" style="font-size: 60px; margin: 25px 0 35px 0; color: #ce9613"></i>
 								<div class="w3-container w3-center" style="height: 40px; font-size: 25px; background-color: #ce9613; color: white;">INTERNET</div>
 							</div>
 							</div>
-							<div class = "w3-col" style = "width:25%;">
+							<div class = "w3-col w3-mobile m3 l3" style = "width:25%;">
 								<div class="w3-card-4 w3-center w3-border w3-animate-left" style="width:240px; height: 160px; background-color: #f7cac9; animation-duration: 1.5s;">					
 								<i class="fa fa-television fa-5x" aria-hidden="true" style="font-size: 80px; margin: 20px 0 19px 0; color: #e65755"></i>		
 								<div class="w3-container w3-center" style="height: 40px; font-size: 25px; background-color: #e65755; color: white;">CABLE</div>	
 								</div>
 							</div>
-							<div class = "w3-col" style = "width:25%;">
+							<div class = "w3-col w3-mobile m3 l3" style = "width:25%;">
 								<div class="w3-card-4 w3-center w3-border w3-animate-left" style="width:240px; height: 160px; background-color:#d2a2c9; animation-duration: 1.5s;">					
 								<i class="fa fa-credit-card fa-5x" aria-hidden="true" style="font-size: 80px; margin: 20px 0 19px 0; color: #854078"></i>		
 								<div class="w3-container w3-center" style="height: 40px; font-size: 25px; background-color: #854078; color: white;">CREDIT CARD</div>	
@@ -130,36 +131,34 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							</div>
 						</div>
 						<div class = "w3-row" style = "margin-top: 50px;">
-							<div class = "w3-col" style = "width:50%;" >
+							<div class = "w3-col w3-mobile m6" style = "width:50%;" >
 								<div id="chart-container">FusionCharts XT will load here!</div>
 							</div>
-							<div class = "w3-col" style = "width:50%;" >
+							<div class = "w3-col w3-mobile m6" style = "width:50%;" >
 								<div class="w3-container w3-animate-top" style = "margin: 30px 0 0 40px; animation-duration: 1.5s;">
 									<div class = "w3-responsive">
 										<table class = "w3-table-all" style = "height: 100px;">
 										<tr>
-  											<th>Points</th>
-  											<th>Points</th>
+  											<th colspan = "2">TUBIG</th>
 										</tr>
 										<tr>
-  											<td>50</td>
-  											<td>50</td>
+  											<td>Due Date:</td>
+  											<td>June 27, 2017</td>
 										</tr>
 										<tr>
-  											<td>94</td>
-  											<td>94</td>
+  											<td>Amount Due:</td>
+  											<td>₱2, 000.00</td>
 										</tr>
 										<tr>
-  											<td>67</td>
-  											<td>67</td>
+  											<td>Consumption:</td>
+  											<td>5, 000 gallons</td>
 										</tr>
 										<tr>
-  											<td>94</td>
-  											<td>94</td>
+  											<td>Reference No.:</td>
+  											<td>5556-8973-23</td>
 										</tr>
 										<tr>
-  											<td>67</td>
-  											<td>67</td>
+  											<td colspan = "2" class= "w3-center"><button class="w3-button w3-black">See More</button></td>
 										</tr>
 									</table>
 								</div>
